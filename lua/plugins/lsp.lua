032e44f786993522c2367efd4322735019967a23
unsigned_var <91784883+unsigned-rbx@users.noreply.github.com>
2025-08-25 19:42:53 +0800

    treesitter fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -350,6 +350,7 @@ return {
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				luau = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
